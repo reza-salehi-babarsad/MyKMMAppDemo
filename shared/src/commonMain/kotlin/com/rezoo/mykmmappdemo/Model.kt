ab91289f669f154c0ac1c9566a0f980a713d0f27
reza <rezasalehibabarsad@hotmail.com>
2024-01-24 15:12:23 +0330

    12

--- shared/src/commonMain/kotlin/com/rezoo/mykmmappdemo/Model.kt
+++ shared/src/commonMain/kotlin/com/rezoo/mykmmappdemo/Model.kt
@@ -5,7 +5,8 @@ data class Model(
     val model2:String,
     val model3:String,val model5:String,
     val model9:String,
-    val model1o:String
+    val model1o:String,
+    val model12:String
 
 )
 
